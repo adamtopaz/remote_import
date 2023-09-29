de6c60a16c75f3139117cb62707bfdc1913059b0
adamtopaz <github@adamtopaz.com>
2023-09-29 10:30:59 -0600

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «remote_import» {
+  -- add package configuration options here
+}
+
+@[default_target]
+lean_lib «RemoteImport» {
+  -- add library configuration options here
+}
